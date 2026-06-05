f46bd950221c2b1c1783c9218fa143016ee44caf
Kang-Woo Lee <kwlee@etri.re.kr>
2026-06-05 14:59:03 +0900

    .

--- src/main/java/mdt/sample/workflow/WfInnercaseDemo.java
+++ src/main/java/mdt/sample/workflow/WfInnercaseDemo.java
@@ -26,29 +26,29 @@ public class WfInnercaseDemo {
 		WorkflowModel wfDesc;
 		
 		wfDesc = new WorkflowModel();
-		wfDesc.setId("innercase-demo-workflow");
+		wfDesc.setId("innercase_demo_workflow");
 		wfDesc.setName("냉장고 내함 두께 불량을 탐지 워크플로우");
 		wfDesc.setDescription("본 워크플로우는 냉장고의 내함 두께 불량을 탐지한다.");
 
 		TaskDescriptor descriptor;
 
 		// UpperImage는 냉장고 내함의 상단 두께 불량을 탐지.
-		descriptor = inspectSurfaceThickness(manager, "inspect-thickness");
+		descriptor = inspectSurfaceThickness(manager, "inspect_thickness");
 		wfDesc.addTaskDescriptor(descriptor);
 
 		// 두께 불량 탐지 결과를 DefectList에 누적.
-		descriptor = updateDefectList(manager, "update-defect-list");
-		descriptor.addDependency("inspect-thickness");
+		descriptor = updateDefectList(manager, "update_defect_list");
+		descriptor.addDependency("inspect_thickness");
 		wfDesc.addTaskDescriptor(descriptor);
 
 		// DefectList를 기반으로 공정 시뮬레이션을 수행하여 평균 사이클 타임을 계산.
-		descriptor = simulateProcess(manager, "simulate-process");
-		descriptor.addDependency("update-defect-list");
+		descriptor = simulateProcess(manager, "simulate_process");
+		descriptor.addDependency("update_defect_list");
 		wfDesc.addTaskDescriptor(descriptor);
 		
 		// Innercase를 구성하는 Heater, Former, Trimmer의 사이클 타임을 활용하여 공정 최적화를 실행.
-		descriptor = optimizeProcess(manager, "optimize-process");
-		descriptor.addDependency("simulate-process");
+		descriptor = optimizeProcess(manager, "optimize_process");
+		descriptor.addDependency("simulate_process");
 		wfDesc.addTaskDescriptor(descriptor);
 
 //		System.out.println(MDTModelSerDe.toJsonString(wfDesc));
@@ -68,7 +68,6 @@ public class WfInnercaseDemo {
 		descriptor.setType(AASOperationTask.class.getName());
 		descriptor.setId(id);
 		descriptor.addInputArgumentSpec("UpperImage", ArgumentSpec.reference("param:inspector:UpperImage"));
-		descriptor.addOutputArgumentSpec("Defect", ArgumentSpec.reference("oparg:inspector:ThicknessInspection:out:Defect"));
 		descriptor.addOption(AASOperationTask.OPTION_OPERATION, opElmRef.toStringExpr());
 		descriptor.addOption(MDTTask.OPTION_TIMEOUT, "1m");
 		descriptor.addOption(MDTTask.OPTION_POLL_INTERVAL, "1s");
@@ -86,7 +85,7 @@ public class WfInnercaseDemo {
 		TaskDescriptor descriptor = new TaskDescriptor();
 		descriptor.setType(AASOperationTask.class.getName());
 		descriptor.setId(id);
-		descriptor.addInputArgumentSpec("Defect", ArgumentSpec.reference("oparg:inspector:ThicknessInspection:out:Defect"));
+		descriptor.addInputArgumentSpec("Defect", ArgumentSpec.taskOutput("inspect_thickness", "Defect"));
 		descriptor.addInputArgumentSpec("DefectList", ArgumentSpec.reference("param:inspector:DefectList"));
 		descriptor.addOutputArgumentSpec("Output", ArgumentSpec.reference("param:inspector:DefectList"));
 		descriptor.addOption(AASOperationTask.OPTION_OPERATION, opElmRef.toStringExpr());
